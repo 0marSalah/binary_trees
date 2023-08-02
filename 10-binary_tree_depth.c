09970c4178e66d4589bfdbcac314ae33cdb4f76a
0marSalah <omar.salah1597@gmail.com>
2023-08-02 18:41:24 +0300

    one line

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,17 +7,5 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-  size_t lh = 0, rh = 0;
-
-  if (tree)
-  {
-    if (tree->parent)
-    {
-      lh = tree->parent->left ? 1 + binary_tree_depth(tree->parent) : 0;
-      rh = tree->parent->right ? 1 + binary_tree_depth(tree->parent) : 0;
-      return ((lh > rh) ? lh : rh);
-    }
-    return (0);
-  }
-  return (0);
+  return (tree && tree->parent ? 1 + binary_tree_depth(tree->parent) : 0);
 }
