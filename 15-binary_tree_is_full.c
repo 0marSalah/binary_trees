ad193450237325cb08b431ad1d95fc5245e8311a
0marSalah <omar.salah1597@gmail.com>
2023-08-03 17:51:30 +0300

    بهء

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,80 +1,36 @@
 #include "binary_trees.h"
 
 /**
+ * is_full_recursive - Checks if a binary tree is full recursively.
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: If tree is not full, 0.
+ *         Otherwise, 1.
+ */
+int is_full_recursive(const binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		if ((tree->left != NULL && tree->right == NULL) ||
+		    (tree->left == NULL && tree->right != NULL) ||
+		    is_full_recursive(tree->left) == 0 ||
+		    is_full_recursive(tree->right) == 0)
+			return (0);
+	}
+	return (1);
+}
+
+/**
  * binary_tree_is_full - Checks if a binary tree is full.
  * @tree: A pointer to the root node of the tree to check.
  *
  * Return: If tree is NULL or is not full - 0.
  *         Otherwise - 1.
  */
-
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-if (tree == NULL)
-return (0);
-
-if (binary_tree_nodes(tree) % 2 == 0)
-return (0);
-
-if (binary_tree_balance(tree) != 0)
-return (0);
-
-return (1);
+	if (tree == NULL)
+		return (0);
+	return (is_full_recursive(tree));
 }
 
-
-/**
- * binary_tree_balance - Measures the balance factor of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the balance factor.
- *
- * Return: If tree is NULL, return 0, else return balance factor.
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-if (tree)
-return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
-
-return (0);
-}
-
-/**
-* binary_tree_height - Measures the height of a binary tree.
-* @tree: A pointer to the root node of the tree to measure the height.
-*
-* Return: If tree is NULL, your function must return 0, else return height.
-*/
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-if (tree)
-{
-size_t l = 0, r = 0;
-
-l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-return ((l > r) ? l : r);
-}
-return (0);
-}
-
-/**
- * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
- * @tree: pointer to the root node of the tree to count the number of nodes
- *
- * Return: number of nodes with at least 1 child in a binary tree
- */
-
-size_t binary_tree_nodes(const binary_tree_t *tree)
-{
-size_t nodes = 0;
-
-if (tree == NULL)
-return (0);
-
-if (tree->left != NULL || tree->right != NULL)
-nodes++;
-
-nodes += binary_tree_nodes(tree->left);
-nodes += binary_tree_nodes(tree->right);
-
-return (nodes);
-}
